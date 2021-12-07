6013051f790e06e3b4b9bca33972fcf0667aaf81
weihao <weihao.li.tw@gmail.com>
2021-12-07 23:24:34 +0800

    [add snowflake]

--- variousconnector/__init__.py
+++ variousconnector/__init__.py
@@ -2,3 +2,7 @@
 from variousconnector.postgresql import (
     postgresql_connector
     )
+
+from variousconnector.snowflake import (
+    snowflake_connector
+    )
--- variousconnector/postgresql.py
+++ variousconnector/postgresql.py
@@ -16,8 +16,9 @@ class postgresql_connector:
             self.pgres_port = credential['port']
             self.psql_user = credential['user']
             self.psql_pass = credential['pwd']
-    
-            if credential['ssh_enable'] is True:
+            self.ssh_enable = credential['ssh_enable']
+            
+            if self.ssh_enable is True:
                 self.ssh_host = credential['ssh_host']
                 self.ssh_user = credential['ssh_user']
                 self.ssh_password = credential['ssh_pwd']
@@ -35,24 +36,32 @@ class postgresql_connector:
                              f"here is an example: \n"
                              f"{credential_example}"
                              )
-            
-        self.server = SSHTunnelForwarder(
-            (self.ssh_host, 22),
-            ssh_username=self.ssh_user,
-            ssh_password=self.ssh_password,
-            remote_bind_address=(self.pgres_host, self.pgres_port),
-            )
-        server = self.server
-        server.start()  # start ssh server
-        self.local_port = server.local_bind_port
-        print(f'Server connected via SSH || Local Port: {self.local_port}...')
+        if credential['ssh_enable'] is True:
+            self.server = SSHTunnelForwarder(
+                (self.ssh_host, 22),
+                ssh_username=self.ssh_user,
+                ssh_password=self.ssh_password,
+                remote_bind_address=(self.pgres_host, self.pgres_port),
+                )
+            server = self.server
+            server.start()  # start ssh server
+            self.local_port = server.local_bind_port
+            print(f'Server connected via SSH || Local Port: {self.local_port}...')
+        else:
+            self.local_port = self.pgres_port
 
     def query(self, db, query):
+    
         psql_user = self.psql_user
         psql_pass = self.psql_pass
-        engine = create_engine(f'postgresql://{psql_user}:{psql_pass}@127.0.0.1:{self.local_port}/{db}')
+        
+        # engine setting
+        engine_setting = f'postgresql+psycopg2://{psql_user}:{psql_pass}@localhost:{self.local_port}/{db}'
+        engine = create_engine(engine_setting)
         print(f'Database [{db}] session created...')
-        self.query_df = pd.read_sql(query, engine)
+        
+        # run query
+        query_result_df = pd.read_sql(query, engine)
         print('<> Query Sucessful <>')
         engine.dispose()
-        return self.query_df
+        return query_result_df
